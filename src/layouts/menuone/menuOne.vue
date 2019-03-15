9fabd68baed889ea0782b8d5f53b4f9073eabe15
Paolo Arena <paolo.arena77@gmail.com>
2019-03-15 12:23:38 +0100

    Menu Style (active, ...) expand-icon-class="my-menu-separat" header-class="my-menu" active-class="my-menu-active"

--- src/layouts/menuone/menuOne.vue
+++ src/layouts/menuone/menuOne.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="list no-border platform-delimiter light-paragraph">
+    <div class="no-border">
         <q-list class="rounded-borders text-primary">
             <template v-for="(parent, index) in getmenu">
                 <div class="q-list-header">{{replaceUnderlineToSpace(index)}}</div>
@@ -10,15 +10,17 @@
                                 :content-inset-level="0.5"
                                 :label="$t(item.name)"
                                 :icon="item.materialIcon"
-                                class="titleSubMenu"
+                                expand-icon-class="my-menu-separat"
+                                header-class="my-menu"
+                                active-class="my-menu-active"
                         >
 
                             <q-expansion-item v-for="(child2, index) in item.routes2" :to="child2.route" :key="index"
                                               :header-inset-level="0.5"
                                               :duration="300"
                                               expand-icon="map"
-                                              :active="child2.label === 'Outbox'"
-                                              class="item item-link drawer-closer cursor-pointer"
+                                              active-class="my-menu-active"
+                                              class="item item-link drawer-closer cursor-pointer my-menu"
                                               :label="$t(child2.name)">
                             </q-expansion-item>
 
@@ -29,14 +31,16 @@
                             <div v-show="true">
                                 <q-item
                                         clickable
+                                        v-ripple
                                         exact
                                         :to="item.route"
-                                        active-class="my-menu-link">
-                                    <q-item-section avatar>
+                                        active-class="my-menu-active"
+                                >
+                                    <q-item-section avatar class="my-menu-icon">
                                         <q-icon :name="item.materialIcon"/>
                                     </q-item-section>
 
-                                    <q-item-section>
+                                    <q-item-section class="my-menu">
                                         {{$t(item.name)}}
                                     </q-item-section>
                                 </q-item>
@@ -44,6 +48,9 @@
                             </div>
                         </q-slide-transition>
                     </div>
+
+                    <!--<q-separator spaced />-->
+
                 </div>
             </template>
         </q-list>
